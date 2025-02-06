60554a3d153f8f62fa7b7b20e47d40c4dd0437d0
Ooratak <alib3hjat1ardakan1@gmail.com>
2025-02-07 00:59:02 +0330

    style: change /start to /check

--- bot.py
+++ bot.py
@@ -10,7 +10,8 @@ TOKEN = os.getenv("TOKEN")
 
 async def help(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('You can use /start to check if the bot is up and running.\nYou can use /get_price {crypto_name} to check the price.')
-async def start(update: Update, context: CallbackContext) -> None:
+
+async def check(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('The bot is up and running.')
 
 def get_crypto_price(crypto: str) -> float:
@@ -33,7 +34,7 @@ async def get_price(update: Update, context: CallbackContext) -> None:
 def main():
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("help", help))
-    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("check", check))
     application.add_handler(CommandHandler("get_price", get_price))
     application.run_polling()
 
