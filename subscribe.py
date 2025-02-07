139be8e5b31b8e03201f35283467ab58acc5703c
Ooratak <alib3hjat1ardakan1@gmail.com>
2025-02-07 07:15:49 +0330

    build: add subscribe.py

---
+++ subscribe.py
@@ -0,0 +1,24 @@
+from telegram import Update
+from bot import get_crypto_price
+
+class Subscribe:
+    crypto: str
+    change_percent: float
+    update: Update
+    initPrice: float
+
+    def __init__(self, crypto: str, change_percent: float, update: Update):
+        self.crypto = crypto
+        self.change_percent = change_percent
+        self.update = update
+        self.initPrice = get_crypto_price(crypto)
+
+    def check_subscribe(self):
+        while True:
+            newPrice = get_crypto_price(self.crypto)
+            if self.initPrice * (100 + self.change_percent) == newPrice:
+                self.update.message.reply_text(f'The new price is {newPrice} USD.')
+                del self
+                return
+
+
