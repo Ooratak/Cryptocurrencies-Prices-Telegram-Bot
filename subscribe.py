13a22cdcb7f93bae00aac5f8711b101d1492b9a8
Ooratak <alib3hjat1ardakan1@gmail.com>
2025-02-07 16:28:55 +0330

    fix: fix subscribe.py

---
+++ subscribe.py
@@ -0,0 +1,27 @@
+import asyncio
+from telegram import Update
+from get_crypto_price import get_crypto_price
+
+class Subscribe:
+    crypto: str
+    changePercent: float
+    update: Update
+    initPrice: float
+
+    def __init__(self, crypto: str, changePercent: float, update: Update):
+        self.crypto = crypto
+        self.changePercent = float(changePercent)
+        self.update = update
+        self.initPrice = get_crypto_price(crypto)
+
+    async def subscribe(self) -> None:
+        while True:
+            newPrice = get_crypto_price(self.crypto)
+            if (self.changePercent >= 0 and self.initPrice * (1 + self.changePercent / 100) <= newPrice) or\
+                (self.changePercent < 0 and newPrice <= self.initPrice * (1 + self.changePercent / 100)):
+                await self.update.message.reply_text(f'The new price is {newPrice} USD.')
+            await asyncio.sleep(1)
+
+    async def unsubscribe(self) -> None:
+        del self
+
