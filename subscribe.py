330021ec303cbb329cdee5befcab123105deb42e
Ooratak <alib3hjat1ardakan1@gmail.com>
2025-02-07 08:01:44 +0330

    fix: fix changePercent type in subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -3,20 +3,20 @@ from get_crypto_price import get_crypto_price
 
 class Subscribe:
     crypto: str
-    change_percent: float
+    changePercent: float
     update: Update
     initPrice: float
 
-    def __init__(self, crypto: str, change_percent: float, update: Update):
+    def __init__(self, crypto: str, changePercent: float, update: Update):
         self.crypto = crypto
-        self.change_percent = change_percent
+        self.changePercent = float(changePercent)
         self.update = update
         self.initPrice = get_crypto_price(crypto)
 
     def check_subscribe(self):
         while True:
             newPrice = get_crypto_price(self.crypto)
-            if self.initPrice * (100 + self.change_percent) == newPrice:
+            if self.initPrice * (100 + self.changePercent) == newPrice:
                 self.update.message.reply_text(f'The new price is {newPrice} USD.')
                 del self
                 return
